3ecc100eadfde972ad463bb9059b773ee08f81ec
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2023-12-12 22:16:45 +0100

    wezterm: fix opt key on macos

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -26,4 +26,7 @@ config.font_size = 16.0
 -- Hide tabbar if only one tab
 config.hide_tab_bar_if_only_one_tab = true
 
+-- Make { } work for scandinavian keyboard layouts on macos
+config.send_composed_key_when_left_alt_is_pressed = true
+
 return config
