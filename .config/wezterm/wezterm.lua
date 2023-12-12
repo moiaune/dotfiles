82fbe673eaf109769ad6abd92a063be9e393056f
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2023-12-12 14:36:52 +0100

    wezterm: trying out a new terminal emulator

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+--- INITIALIZE CONFIG
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- CONFIGURATION
+
+-- Initial size
+config.initial_cols = 80
+config.initial_rows = 30
+
+-- Launch tmux on startup
+config.default_prog = { '/usr/local/bin/tmux', '-u' }
+
+-- Default color scheme
+config.color_scheme = 'Everforest Dark (Gogh)'
+
+-- Default font
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font_size = 16.0
+
+-- Hide tabbar if only one tab
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
