ecd39755976389779ee9c82ffe9116ca194669d1
Mads Moi-Aune <609372+moiaune@users.noreply.github.com>
2024-07-04 15:22:58 +0200

    powershell: add XDG_CONFIG_HOME and change oh-my-posh config path

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,24 @@
-using namespace System.Management.Automation
-using namespace System.Management.Automation.Language
-
 # -----------------------------------------------------------------------------
 #     - MODULES -
 # -----------------------------------------------------------------------------
 
-# Import-Module -Name Get-ChildItemColor
 Import-Module -Name PSFzf
 Import-Module -Name posh-git
+
+if ($IsWindows) {
+    $env:XDG_CONFIG_HOME = "$env:USERPROFILE/.config"
+    if (-not(Test-Path -Path $env:XDG_CONFIG_HOME -PathType Container)) {
+        New-Item -Path $env:XDG_CONFIG_HOME -ItemType Directory
+    }
+} else {
+    $env:XDG_CONFIG_HOME = "$env:HOME/.config"
+}
 
 # -----------------------------------------------------------------------------
 #     - PROMPT -
 # -----------------------------------------------------------------------------
 
-oh-my-posh init pwsh --config "~/.oh-my-posh/themes/google.omp.yml" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:XDG_CONFIG_HOME/oh-my-posh/config.yml" | Invoke-Expression
 
 # -----------------------------------------------------------------------------
 #     - ENVIRONMENT VARIABLES -
@@ -76,57 +81,12 @@ Set-PSReadLineOption -AddToHistoryHandler {
     return ($line -notmatch $sensitive)
 }
 
-# -----------------------------------------------------------------------------
-#     - TERMINAL COLORS -
-#     https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/using-light-theme?view=powershell-7.3
-# -----------------------------------------------------------------------------
-
-# $ISETheme = @{
-#     Command                = $PSStyle.Foreground.FromRGB(0x0000FF)
-#     Comment                = $PSStyle.Foreground.FromRGB(0x006400)
-#     ContinuationPrompt     = $PSStyle.Foreground.FromRGB(0x0000FF)
-#     Default                = $PSStyle.Foreground.FromRGB(0x0000FF)
-#     Emphasis               = $PSStyle.Foreground.FromRGB(0x287BF0)
-#     Error                  = $PSStyle.Foreground.FromRGB(0xE50000)
-#     InlinePrediction       = $PSStyle.Foreground.FromRGB(0x93A1A1)
-#     Keyword                = $PSStyle.Foreground.FromRGB(0x00008b)
-#     ListPrediction         = $PSStyle.Foreground.FromRGB(0x06DE00)
-#     Member                 = $PSStyle.Foreground.FromRGB(0x000000)
-#     Number                 = $PSStyle.Foreground.FromRGB(0x800080)
-#     Operator               = $PSStyle.Foreground.FromRGB(0x757575)
-#     Parameter              = $PSStyle.Foreground.FromRGB(0x000080)
-#     String                 = $PSStyle.Foreground.FromRGB(0x8b0000)
-#     Type                   = $PSStyle.Foreground.FromRGB(0x008080)
-#     Variable               = $PSStyle.Foreground.FromRGB(0xff4500)
-#     ListPredictionSelected = $PSStyle.Background.FromRGB(0x93A1A1)
-#     Selection              = $PSStyle.Background.FromRGB(0x00BFFF)
-# }
-# Set-PSReadLineOption -Colors $ISETheme
-
-# # Improve light theme in terminal
-# $PSStyle.Formatting.FormatAccent       = "`e[32m"
-# $PSStyle.Formatting.TableHeader        = "`e[32m"
-# $PSStyle.Formatting.ErrorAccent        = "`e[36m"
-# $PSStyle.Formatting.Error              = "`e[31m"
-# $PSStyle.Formatting.Warning            = "`e[33m"
-# $PSStyle.Formatting.Verbose            = "`e[33m"
-# $PSStyle.Formatting.Debug              = "`e[33m"
-# $PSStyle.Progress.Style                = "`e[33m"
-# $PSStyle.FileInfo.Directory            = $PSStyle.Background.FromRgb(0x2f6aff) + $PSStyle.Foreground.BrightWhite
-# $PSStyle.FileInfo.SymbolicLink         = "`e[36m"
-# $PSStyle.FileInfo.Executable           = "`e[95m"
-# $PSStyle.FileInfo.Extension['.ps1']    = "`e[36m"
-# $PSStyle.FileInfo.Extension['.ps1xml'] = "`e[36m"
-# $PSStyle.FileInfo.Extension['.psd1']   = "`e[36m"
-# $PSStyle.FileInfo.Extension['.psm1']   = "`e[36m"
-
 Set-PSReadLineOption -Colors @{ Member = "`e[95m"; Number = "`e[95m" }
 Set-PSReadLineOption -Colors @{
     Member    = "`e[95m"
     Parameter = "`e[97m"
     Number    = "`e[97m"
 }
-
 
 # -----------------------------------------------------------------------------
 #     - ALIASES -
@@ -137,7 +97,7 @@ Set-Alias -Name ll -Value ListFilesAndFolders
 function ListAllFilesAndFolders { param([string]$path = ".") Get-ChildItem -Path $path -Force }
 Set-Alias -Name la -Value ListAllFilesAndFolders
 
-function ListAllFilesAndFoldersSorted { param([string]$path = ".") Get-ChildItem -Path $path -Force | Sort-Object LastWriteTime }
+function ListAllFilesAndFoldersSorted { param([string]$path = ".") Get-ChildItem -Path $path -Force | Sort-Object LastWriteTime }
 Set-Alias -Name lss -Value ListAllFilesAndFoldersSorted
 
 function GitCommitAlias { git commit -m $args[0] }
